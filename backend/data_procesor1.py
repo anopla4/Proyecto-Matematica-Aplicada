66eee4810d6fa69e9ceacc7cf8ec925549c2796a
ams1927 <abel.molina@estudiantes.matcom.uh.cu>
2021-11-04 22:49:57 -0400

    feat:dataParsedAndStandarized

--- backend/data_procesor1.py
+++ backend/data_procesor1.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
 
 def municipality_parser_universityDistance(municipality_column):
     dict_munic = {"plaza de la revolucion" : 0,
@@ -23,3 +26,24 @@ def municipality_parser_universityDistance(municipality_column):
     for munc in municipality_column:
         result.append(dict_munic[munc.lower()]/6)
     return result
+
+dict_parsers_global = {"municipality_parser_universityDistance": municipality_parser_universityDistance()}
+
+
+def data_performing(df, interesting_atribute, weights, dict_parser_strategy):
+    """
+    df-> data frama de pandas(data de entrada completa)
+    interesting_atribute-> lista booleana(True los atributos relevantes)
+    weitght -> relevancia de los atributos(0 los no relevantes)
+    """
+    relevant_data = df.loc[: ,interesting_atribute]
+    for atribute_name in dict_parser_strategy:
+        relevant_data[atribute_name] = dict_parsers_global[dict_parser_strategy[atribute_name]](relevant_data[atribute_name])
+    relevant_weights = weights[interesting_atribute]
+    relevant_data = pd.get_dummies(relevant_data)
+    sc = StandardScaler()
+    relevant_data[:,:] = sc.fit_transform(relevant_data[:,:])
+    for i in range(len(relevant_weights)):
+        relevant_data.loc[:,i] *= relevant_weights[i]
+    return relevant_data
+
