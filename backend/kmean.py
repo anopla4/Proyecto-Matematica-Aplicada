06b29eef489ca67bbd168ab55a8d0ba4344e266e
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2021-11-16 03:18:47 -0800

    fix: kmean method name

--- backend/kmean.py
+++ backend/kmean.py
@@ -2,33 +2,34 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.cluster import KMeans
 
-def Get_Groups_with_kmean(dataset, number_of_groups):
+
+def get_groups_with_kmean(dataset, number_of_groups):
     """
     Se recibe un data frame(dataset), correspondiente a un subgrupo determinado de estudiantes, con los atributos normalizados
     y con el procesamiento correspondiente, además del número de grupos en que se desean repartir. Se aplica el algortimo de
-    kmean para agrupar estos estudiantes en grupos afines(clusterizar) y a partir de estos grupos afines se reparten 
+    kmean para agrupar estos estudiantes en grupos afines(clusterizar) y a partir de estos grupos afines se reparten
     de forma equitativa entre los grupos a crear
     """
-    kmeans = KMeans(n_clusters= number_of_groups*2, init='k-means++')
+    kmeans = KMeans(n_clusters=number_of_groups * 2, init="k-means++")
     kmeans.fit(dataset)
     clusters = {}
-    for i,label in enumerate(kmeans.labels_):
+    for i, label in enumerate(kmeans.labels_):
         if label not in clusters:
             clusters[label] = []
         clusters[label].append(i)
     groups = [[] for _ in range(number_of_groups)]
     for i in clusters:
-        k = int(len(clusters[i])/number_of_groups)
+        k = int(len(clusters[i]) / number_of_groups)
         for j in range(number_of_groups):
-            groups[j].extend(clusters[i][(j*k):((j+1)*k)])
+            groups[j].extend(clusters[i][(j * k) : ((j + 1) * k)])
         if k == 0:
             R = len(clusters[i])
         else:
-            R = len(clusters[i])%number_of_groups
+            R = len(clusters[i]) % number_of_groups
         for j in range(R):
             min_group = groups[0]
             for group in groups:
                 if len(group) < len(min_group):
                     min_group = group
-            min_group.append(clusters[i][-(j+1)])
+            min_group.append(clusters[i][-(j + 1)])
     return groups
--- backend/main.py
+++ backend/main.py
@@ -3,33 +3,44 @@ import numpy as np
 from data_processing import data_performing
 from kmean import Get_Groups_with_kmean
 
-data = pd.read_excel('1RO CC 2021.xlsx')
-names = np.array(data["Nombre"] +  " " + data["Apellidos"])
+data = pd.read_excel("1RO CC 2021.xlsx")
+names = np.array(data["Nombre"] + " " + data["Apellidos"])
 
-data_transf = data_performing(data, ['Provincia', 'Municipio',
-       'Situación académica', 'Fuente de Ingreso', 'Organización política', 'Sexo',
-       'Color de la piel', 'Edad'], [1,1,1,1,1,1,1,1], {"Municipio": 'municipality_raw_parser'})
+data_transf = data_performing(
+    data,
+    [
+        "Provincia",
+        "Municipio",
+        "Situación académica",
+        "Fuente de Ingreso",
+        "Organización política",
+        "Sexo",
+        "Color de la piel",
+        "Edad",
+    ],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    {"Municipio": "municipality_raw_parser"},
+)
 
 groups = Get_Groups_with_kmean(data_transf, 6)
 aux = 0
-for i,group in enumerate(groups):
+for i, group in enumerate(groups):
     m = 0
     f = 0
     becados = 0
     comunistas = 0
     print(f"Grupo {i+1}")
-    for number in (group):
-        if data["Sexo"][number] == 'Masculino':
+    for number in group:
+        if data["Sexo"][number] == "Masculino":
             m += 1
         else:
             f += 1
-        if data["Provincia"][number] != "La Habana": 
+        if data["Provincia"][number] != "La Habana":
             becados += 1
-        if data["Organización política"][number] == "UJC": 
+        if data["Organización política"][number] == "UJC":
             comunistas += 1
         print(names[number])
     print(f"{m} Hombres")
     print(f"{f} Mujeres")
     print(f"{becados} Becados")
     print(f"{comunistas} UJC")
-
