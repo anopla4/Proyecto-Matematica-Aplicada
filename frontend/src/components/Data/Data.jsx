3d8305b8e6994d5b807900160d2c7642860527b5
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2021-11-23 07:58:08 -0800

    fix: DropdownMultiSelect Select/Deselect All behavior

--- frontend/src/components/Data/Data.jsx
+++ frontend/src/components/Data/Data.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
-import { Table, Row, Col, Form, Toast, Button } from "react-bootstrap";
+import {
+  Table,
+  Row,
+  Col,
+  Form,
+  Toast,
+  Button,
+  Container,
+} from "react-bootstrap";
 import DropdownMultiselect from "react-multiselect-dropdown-bootstrap";
+import { withRouter } from "react-router-dom";
+
 class Data extends Component {
   state = {
     data: {
@@ -12,14 +22,18 @@ class Data extends Component {
       Provincia: ["La Habana", "La Habana"],
     },
     showedAttributes: [],
+    attributesType: {},
+    attrTypeIncomplete: false,
     numberOfAttributesToBig: false,
   };
 
   componentWillMount() {
     let attr = Object.keys(this.state.data);
+    let attrType = {};
+    attr.map(x => (attrType[x] = ""));
     let showedAttr =
       attr.length < 5 ? attr.splice(0, attr.length - 1) : attr.splice(0, 5);
-    this.setState({ showedAttributes: showedAttr });
+    this.setState({ showedAttributes: showedAttr, attributesType: attrType });
   }
 
   transformData = function (data) {
@@ -31,13 +45,19 @@ class Data extends Component {
     return m[0].map((_, colIndex) => m.map(row => row[colIndex]));
   };
 
-  handleSelectAttributes = function (e) {
-    if (e.length > 5) {
+  handleSelectAttributes = e => {
+    if (e.length === 0) {
+      let temp = [this.state.showedAttributes[0]];
+      this.setState({
+        showedAttributes: temp,
+        numberOfAttributesToBig: false,
+      });
+    } else if (e.length > 5) {
       this.setState({ numberOfAttributesToBig: true });
     } else {
       this.setState({ showedAttributes: e, numberOfAttributesToBig: false });
     }
-  }.bind(this);
+  };
 
   handleFileUpload = function (e) {
     e.preventDefault();
@@ -51,13 +71,41 @@ class Data extends Component {
   }.bind(this);
 
   handleClickContinue = () => {
-    this.props.history.push({ pathname: "/subset" });
+    if (Object.values(this.state.attributesType).includes("")) {
+      this.setState({ attrTypeIncomplete: true });
+    } else {
+      this.props.history.push({
+        pathname: "/subset",
+        data: {
+          data: this.state.data,
+          attributesType: this.state.attributesType,
+        },
+      });
+    }
+  };
+
+  handleTypeAttributes = e => {
+    let attr = e.target.id;
+    let selectedType = e.target.options[e.target.selectedIndex].innerText;
+    let newAttrType = this.state.attributesType;
+    newAttrType[attr] = selectedType;
+    this.setState({ attributesType: newAttrType });
+  };
+
+  handleCloseToast = () => {
+    this.setState({ attrTypeIncomplete: false });
   };
 
   render() {
     return (
-      <Row>
+      <Row style={{ margin: "3%" }}>
         <Row>
+          <Col className="d-flex align-items-center justify-content-center">
+            <h3>Cargar datos</h3>
+          </Col>
+        </Row>
+        <Row>
+          <h4>Seleccione los datos que quiere agrupar:</h4>
           <Form.Group
             style={{ width: "30%" }}
             controlId="formFile"
@@ -106,11 +154,46 @@ class Data extends Component {
               options={Object.keys(this.state.data)}
               name="attributes"
             />
+            <Container className="mt-5">
+              <h6>Seleccione el tipo de atributo en cada caso</h6>
+              {Object.keys(this.state.data).map(x => (
+                <Row className="mt-2">
+                  <p>{x}: </p>
+                  <Form>
+                    <Form.Select
+                      onChange={this.handleTypeAttributes}
+                      id={x}
+                      aria-label="attribute type"
+                    >
+                      <option></option>
+                      <option value="1">Nominal</option>
+                      <option value="2">Numérico</option>
+                    </Form.Select>
+                  </Form>
+                </Row>
+              ))}
+            </Container>
           </Col>
         </Row>
         <Row>
           <Col className="d-flex align-items-end justify-content-end">
-            <Button onClick={this.handleClickContinue}>Continuar</Button>
+            <Row>
+              {this.state.attrTypeIncomplete && (
+                <Toast className="mt-3 mb-5" onClose={this.handleCloseToast}>
+                  <Toast.Header>
+                    <strong className="me-auto">¡Atención!</strong>
+                  </Toast.Header>
+                  <Toast.Body>
+                    Debe seleccionar un tipo para todos los atributos.
+                  </Toast.Body>
+                </Toast>
+              )}
+            </Row>
+            <Row>
+              <Button className="mt-5" onClick={this.handleClickContinue}>
+                Continuar
+              </Button>
+            </Row>
           </Col>
         </Row>
       </Row>
@@ -118,4 +201,4 @@ class Data extends Component {
   }
 }
 
-export default Data;
+export default withRouter(Data);
