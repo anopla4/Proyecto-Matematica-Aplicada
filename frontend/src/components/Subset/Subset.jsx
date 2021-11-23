dfa0a1b4f55ddcd9d6b6a8fe3e26cb8712133b68
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2021-11-23 08:01:11 -0800

    feat: Create and show subsets

--- frontend/src/components/Subset/Subset.jsx
+++ frontend/src/components/Subset/Subset.jsx
@@ -1,16 +1,333 @@
 import React, { Component } from "react";
-import { Col, Row, Button } from "react-bootstrap";
+import {
+  Col,
+  Row,
+  Button,
+  Form,
+  ListGroup,
+  Badge,
+  Tabs,
+  Tab,
+} from "react-bootstrap";
+import BootstrapTable from "react-bootstrap-table-next";
+import filterFactory, {
+  numberFilter,
+  textFilter,
+} from "react-bootstrap-table2-filter";
+import { withRouter } from "react-router-dom";
+import DropdownMultiselect from "react-multiselect-dropdown-bootstrap";
 
 class Subset extends Component {
-  state = { numberOfGroups: 3, students: [], attributes: [], restrictions: [] };
+  state = {
+    numberOfGroups: 1,
+    numberOfGroupsSubset: 1,
+    data: undefined,
+    attributesType: undefined,
+    columns: undefined,
+    selected: [],
+    numberRows: 0,
+    createSubset: false,
+    subsetAttributes: [],
+    studentsSubset: [],
+    subsets: {},
+    subsetToBeAdded: 0,
+  };
+
+  componentWillMount() {
+    let data = this.props.location.data.data;
+    let rows = Object.values(data)[0].length;
+    let stdSub = { 0: [] };
+    for (let index = 0; index < rows; index++) {
+      stdSub[0].push(index);
+    }
+    let attributesType = this.props.location.data.attributesType;
+    let col = [];
+    Object.keys(attributesType).map(x => {
+      col.push({
+        dataField: x,
+        text: x,
+        headerFormatter: (column, colIndex, components) => {
+          return (
+            <div>
+              {column.text} {components.filterElement}
+            </div>
+          );
+        },
+        filter:
+          this.props.location.data.attributesType[x] === "Nominal"
+            ? textFilter()
+            : numberFilter(),
+      });
+      return 0;
+    });
+    this.setState({
+      data: data,
+      attributesType: attributesType,
+      columns: col,
+      numberRows: rows,
+      subsets: stdSub,
+    });
+  }
+
+  componentDidMount() {
+    while (document.querySelector(".sr-only") !== null) {
+      var elem = document.querySelector(".sr-only");
+      elem.parentNode.removeChild(elem);
+    }
+  }
 
   handleGenerateGroups = () => {
     this.props.history.push({ pathname: "/groups" });
   };
 
+  transformData = function (data) {
+    let m = [];
+    let showedAttr = Object.entries(data);
+    showedAttr.map(x => m.push(x[1]));
+    let rows = m[0].map((_, colIndex) => m.map(row => row[colIndex]));
+    let sol = [];
+    rows.forEach((row, j) => {
+      let obj = { id: j };
+      let attr = Object.keys(this.state.attributesType);
+      row.map((item, index) => (obj[attr[index]] = item));
+      sol.push(obj);
+    });
+    return sol;
+  };
+
+  handleOnSelect = (row, isSelect) => {
+    if (isSelect) {
+      this.setState(() => ({
+        selected: [...this.state.selected, row.id],
+      }));
+    } else {
+      this.setState(() => ({
+        selected: this.state.selected.filter(x => x !== row.id),
+      }));
+    }
+  };
+
+  handleOnSelectAll = (isSelect, rows) => {
+    const ids = rows.map(r => r.id);
+    if (isSelect) {
+      this.setState(() => ({
+        selected: ids,
+      }));
+    } else {
+      this.setState(() => ({
+        selected: [],
+      }));
+    }
+  };
+
+  handleChangeNumberGroups = e => {
+    this.setState({ numberOfGroups: e.target.value });
+  };
+
+  handleChangeNumberGroupsSubset = e => {
+    this.setState({ numberOfGroupsSubset: e.target.value });
+  };
+
+  handleCreateSubset = () => {
+    this.setState({ createSubset: true });
+  };
+
+  handleAcceptSubset = () => {
+    let newSubset = {};
+    newSubset["attributes"] = [...this.state.subsetAttributes];
+    newSubset["numberOfGroups"] = [...this.state.numberOfGroupsSubset];
+    newSubset["students"] = [];
+    let newSubsets = this.state.subsets;
+    newSubsets[Object.keys(this.state.subsets).length] = newSubset;
+    this.setState({
+      createSubset: false,
+      subsetAttributes: [],
+      numberOfGroupsSubset: 1,
+      subsets: newSubsets,
+    });
+  };
+
+  handleSelectAttributes = e => {
+    let attr = [];
+    e.map(x => attr.push([x, 1]));
+    this.setState({ subsetAttributes: attr });
+  };
+
+  handleImportanceAttribute = e => {
+    let subsetAttr = [...this.state.subsetAttributes];
+    subsetAttr[e.target.id] = parseInt(e.target.value);
+    this.setState({ subsetAttributes: subsetAttr });
+  };
+
+  handleOnChangeSubsetToBeAdded = e => {
+    this.setState({ subsetToBeAdded: e.target.value });
+  };
+
+  handleAssignStudentsToSubset = e => {
+    if (this.state.subsetToBeAdded === 0) {
+      return;
+    } else {
+      let subs = this.state.subsets;
+      subs[this.state.subsetToBeAdded]["students"] = subs[
+        this.state.subsetToBeAdded
+      ]["students"].concat(this.state.selected);
+      subs[0] = subs[0].filter(x => !this.state.selected.includes(x));
+      let newSelected = this.state.selected;
+      newSelected = newSelected.filter(
+        x => !subs[this.state.subsetToBeAdded]["students"].includes(x)
+      );
+      this.setState({ subsets: subs, selected: newSelected });
+    }
+  };
+
+  handleChangeSubset = () => {};
+
   render() {
     return (
-      <Row>
+      <Row style={{ margin: "3%" }}>
+        <Row>
+          <Col className="d-flex align-items-center justify-content-center">
+            <h3>Configurar subconjuntos</h3>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col md={3}>
+            <h6>Elija la cantidad de grupos total que quiere: </h6>
+          </Col>
+          <Col md={1}>
+            <Form>
+              <Form.Control
+                onChange={this.handleChangeNumberGroups}
+                min={1}
+                type="number"
+              ></Form.Control>
+            </Form>
+          </Col>
+        </Row>
+        <Tabs id="subsets-tabs" defaultActiveKey={"0"} className="mb-3 mt-5">
+          {Object.keys(this.state.subsets).map(x => (
+            <Tab
+              eventKey={x}
+              title={x === "0" ? "Sin asignar" : `Subconjunto ${x}`}
+            >
+              <Row>
+                <Col>
+                  <Row>
+                    <BootstrapTable
+                      keyField="id"
+                      data={this.transformData(this.state.data).filter(y => {
+                        if (x === "0") {
+                          return this.state.subsets[x].includes(y.id);
+                        }
+                        return this.state.subsets[x]["students"].includes(y.id);
+                      })}
+                      columns={this.state.columns}
+                      filter={filterFactory()}
+                      selectRow={{
+                        mode: "checkbox",
+                        clickToSelect: true,
+                        selected: this.state.selected,
+                        onSelect: this.handleOnSelect,
+                        onSelectAll: this.handleOnSelectAll,
+                      }}
+                      style={{ overflowX: "scroll" }}
+                    />
+                  </Row>
+                  {x === "0" && (
+                    <Row>
+                      <Col md={8}>
+                        <Badge bg="light">
+                          <Row>
+                            <Col md={9}>
+                              <Button
+                                onClick={this.handleAssignStudentsToSubset}
+                                variant="secondary"
+                              >
+                                Asignar estudiantes al subconjunto{" "}
+                              </Button>
+                            </Col>
+                            <Col md={3}>
+                              <Form.Control
+                                onChange={this.handleOnChangeSubsetToBeAdded}
+                                min={0}
+                                max={this.state.subsets.length}
+                                type="number"
+                              />
+                            </Col>
+                          </Row>
+                        </Badge>
+                      </Col>
+                    </Row>
+                  )}
+                </Col>
+                <Col md={3}>
+                  <Row style={{ paddingLeft: "20%" }}>
+                    <Button
+                      style={{ width: "70%", float: "right" }}
+                      onClick={this.handleCreateSubset}
+                      variant="success"
+                    >
+                      Crear subconjunto
+                    </Button>
+                  </Row>
+                  {this.state.createSubset && (
+                    <Row style={{ paddingLeft: "10%" }} className="mt-5">
+                      <Form>
+                        <Form.Label>
+                          <h6>
+                            Elija la cantidad de grupos para este subconjunto
+                          </h6>
+                        </Form.Label>
+                        <Form.Control
+                          onChange={this.handleChangeNumberGroupsSubset}
+                          min={1}
+                          type="number"
+                          style={{ width: "20%" }}
+                        />
+                        <Form.Label className="mt-3">
+                          <h6>
+                            Elija los atributos importantes para este
+                            subconjunto
+                          </h6>
+                        </Form.Label>
+                        <DropdownMultiselect
+                          style={{ width: "40%" }}
+                          placeholder="Seleccione los atributos relevantes..."
+                          handleOnChange={this.handleSelectAttributes}
+                          options={Object.keys(this.state.data)}
+                          name="attributes"
+                        />
+                      </Form>
+                      <ListGroup className="mt-3">
+                        {this.state.subsetAttributes.map(x => (
+                          <ListGroup.Item>
+                            {x[0]}{" "}
+                            <Form.Range
+                              id={x[0]}
+                              defaultValue={x[1]}
+                              min={0.1}
+                              max={10}
+                              onChange={this.handleImportanceAttribute}
+                            />
+                          </ListGroup.Item>
+                        ))}
+                      </ListGroup>
+                      <Button
+                        style={{ width: "25%" }}
+                        variant="primary"
+                        onClick={this.handleAcceptSubset}
+                        className="mt-5"
+                      >
+                        Aceptar
+                      </Button>
+                    </Row>
+                  )}
+                </Col>
+              </Row>
+            </Tab>
+          ))}
+        </Tabs>
+
         <Row>
           <Col className="d-flex align-items-end justify-content-end">
             <Button onClick={this.handleGenerateGroups}>Generar grupos</Button>
@@ -21,4 +338,4 @@ class Subset extends Component {
   }
 }
 
-export default Subset;
+export default withRouter(Subset);
