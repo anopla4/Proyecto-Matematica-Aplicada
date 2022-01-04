dd4d281de446314d99a5cbdf2801009b1ff516e3
ams1927 <abel.molina@estudiantes.matcom.uh.cu>
2022-01-04 12:17:14 -0500

    feat: add menu contextual

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,16 +1,33 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 const layout = props => {
   return (
-    <Container
-      style={{
-        padding: "0px",
-        margin: "0px",
-        maxWidth: "inherit",
-      }}
-    >
-      <main>{props.children}</main>
+    <Container style={{
+      padding: "0px",
+      margin: "0px",
+      maxWidth: "inherit",}}>
+      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand href="/">KGrupos</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href='/data'>Comenzar</Nav.Link>
+              <Nav.Link href="/guia">Guia</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Container style={{
+          padding: "0px",
+          margin: "0px",
+          paddingTop: "40px",
+          maxWidth: "inherit",
+        }}
+      >  
+          <main>{props.children}</main>
+      </Container>
     </Container>
   );
 };
