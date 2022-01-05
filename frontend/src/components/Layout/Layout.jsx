9a18ec50aed56cf41d590776303b4477439c82ab
Ana Paula <ana.arguelles@estudiantes.matcom.uh.cu>
2022-01-05 13:00:42 -0800

    fix: Guía word in top navbar

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -3,30 +3,38 @@ import { Container, Navbar, Nav } from "react-bootstrap";
 
 const layout = props => {
   return (
-    <Container style={{
-      padding: "0px",
-      margin: "0px",
-      maxWidth: "inherit",}}>
+    <Container
+      style={{
+        padding: "0px",
+        margin: "0px",
+        maxWidth: "inherit",
+        maxHeight: "inherit",
+      }}
+    >
       <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
         <Container>
           <Navbar.Brand href="/">KGrupos</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          <Navbar.Collapse
+            className="justify-content-end"
+            id="basic-navbar-nav"
+          >
             <Nav className="mr-auto">
-              <Nav.Link href='/data'>Comenzar</Nav.Link>
-              <Nav.Link href="/guia">Guia</Nav.Link>
+              <Nav.Link href="/data">Comenzar</Nav.Link>
+              <Nav.Link href="/guia">Guía</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Container style={{
+      <Container
+        style={{
           padding: "0px",
           margin: "0px",
           paddingTop: "40px",
           maxWidth: "inherit",
         }}
-      >  
-          <main>{props.children}</main>
+      >
+        <main>{props.children}</main>
       </Container>
     </Container>
   );
